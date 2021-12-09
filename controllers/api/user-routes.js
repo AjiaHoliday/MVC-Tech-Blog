3f4c3ae6f8ec3285e48249eb429b61d8a47cbf26
ajia <ajialeeholiday@gmail.com>
2021-12-09 15:53:53 -0700

    initial directory structure, models, and controllers

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {User, Blog, Comment} = require('../../models');
+
+// GET /api/user5s
+router.get('/', (req, res)=> {
+
+});
+
+// Get specific user
+router.get('/:id', (req, res) => {
+
+});
+
+// POST users
+router.post('/',(req, res) => {
+    
+});
+
+// update user
+router.put('/:id', (req, res) => {
+
+});
+
+// delete user
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
