576b5c9814933c413a0feb020708360a7f2bb9df
ajia <ajialeeholiday@gmail.com>
2021-12-09 18:24:33 -0700

    hashed password and login logic

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,9 +63,31 @@ router.post('/',(req, res) => {
         });
 });
 
+// Login
+router.post('/login', (req, res) => {
+    User.findOne({
+        where:{
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({message:'No user with this email address'});
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if(!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!'});
+            return;
+        }
+        res.json({user: dbUserData, message: 'You are now logged in!'});
+    });
+});
+
 // update user
 router.put('/:id', (req, res) => {
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
