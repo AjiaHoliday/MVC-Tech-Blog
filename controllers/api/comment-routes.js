ce7700067a5f8bd0d5928a63159b466b6ef50d16
ajia <ajialeeholiday@gmail.com>
2021-12-10 17:40:53 -0700

    single blog page and comment functionality

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -25,16 +25,18 @@ router.get('/', (req, res)=> {
 
 // POST new Comment
 router.post('/',(req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        blog_id: req.body.blog_id
-    })
-        .then(dbCommentData=> res.json(dbCommentData))
-        .catch(err =>{
-            console.log(err);
-            res.status(400).json(err);
-        });
+    if(req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            user_id: req.session.user_id,
+            blog_id: req.body.blog_id
+        })
+            .then(dbCommentData=> res.json(dbCommentData))
+            .catch(err =>{
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
 });
 
 // delete Comment
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,4 +41,44 @@ router.get('/login', (req,res) => {
     res.render('login');
 });
 
+router.get('/blog/:id', (req,res) => {
+    Blog.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'title', 'blog_content', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'blog_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbBlogData => {
+            if(!dbBlogData) {
+                res.status(404).json({ message: 'No blog found with this id'});
+                return;
+            }
+            // serialize t6he data
+            const blog = dbBlogData.get({plain:true});
+
+            res.render('single-blog', {
+                blog,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
