abf259ac9f7a46ccd0532cbb2764726bc72412fd
ajia <ajialeeholiday@gmail.com>
2021-12-12 21:30:31 -0700

    IT WORKS - timer logout functionality

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -10,46 +10,6 @@ async function logout() {
     alert(response.statusText);
   }
 }
-// logout timer
 
-
-var warningTimeout = 15000;
-var timeoutNow = 10000;
-var warningTimerID, timeoutTimerID
-
-function startTimer() {
-  warningTimerID = window.setTimeout(warningInactive, warningTimeout);
-}
-
-window.onmousemove = resetTimer; // catches mouse movements
-window.onmousedown = resetTimer; // catches mouse movements
-window.onclick = resetTimer;     // catches mouse clicks
-window.onscroll = resetTimer;    // catches scrolling
-window.onkeypress = resetTimer;  //catches keyboard actions
-
-function warningInactive() {
-  window.clearTimeout(warningTimerID);
-  timeoutTimerID = window.setTimeout(logout, timeoutNow);
-  $(".modal").addClass("is-active");
-}
-
-function resetTimer() {
-    window.clearTimeout(timeoutTimerID); 
-    window.clearTimeout(warningTimerID);
-    startTimer(); 
-}
-
-// close modal & restart timer
-$(".modal-close").click(function() {
-  $(".modal").removeClass("is-active")
-  resetTimer();
-});
-
-$("#warningBtn").click(function() {
-  $('.modal').removeClass("is-active")
-  resetTimer();
-});
-
-startTimer()
 
 document.querySelector('#logout').addEventListener('click', logout);
---
+++ public/javascript/timeout.js
@@ -0,0 +1,62 @@
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' }
+    });
+  
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+}
+// logout timer
+function showWarning() {
+    console.log('should be showing warning');
+  var warning = document.getElementById('timeoutWarning');
+  warning.classList.add("is-active");
+}
+
+var timoutWarning = 14*60*1000; // Display warning in 14 Mins.
+var timoutNow = 60+1000; // Warning has been shown, give the user 1 minute to interact
+
+var warningTimer;
+var timeoutTimer;
+
+// Start warning timer.
+function StartWarningTimer() {
+    warningTimer = setTimeout(IdleWarning, timoutWarning);
+}
+
+// Reset timers.
+function ResetTimeOutTimer() {
+    clearTimeout(timeoutTimer);
+    StartWarningTimer();
+}
+
+// Show idle timeout warning dialog.
+function IdleWarning() {
+    clearTimeout(warningTimer);
+    timeoutTimer = setTimeout(logout, timoutNow);
+    showWarning();
+}
+
+function activityTracker () {
+  window.onmousemove = ResetTimeOutTimer; // catches mouse movements
+  window.onmousedown = ResetTimeOutTimer; // catches mouse movements
+  window.onclick = ResetTimeOutTimer;     // catches mouse clicks
+  window.onscroll = ResetTimeOutTimer;    // catches scrolling
+  window.onkeypress = ResetTimeOutTimer;  //catches keyboard actions
+}
+
+// close modal & restart timer
+function hideWarning() {
+  var warning = document.getElementById('timeoutWarning');
+  warning.classList.remove("is-active");
+  ResetTimeOutTimer();
+}
+
+document.querySelector(".modal-close").addEventListener('click', hideWarning);
+document.querySelector(".modal-background").addEventListener('click', hideWarning);
+
+StartWarningTimer();
